02e68874ec8485b96a23cc376528d1c31ceec91a
TimNBarlow <TimNBarlow@gmail.com>
2015-01-30 11:31:26 -0500

    DiceBag Object

---
+++ DiceBag/DiceBag.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceBag
+{
+    class DiceBag
+    {
+        //private members
+        private string log;
+        private Random rand;
+        //private functions
+        private void Log();
+
+        //public functions
+        public int Roll(int d);
+        public int Roll(int d, int n);
+        public int RollMod(int d, int mod);
+        public int RollMod(int d, int mod, int n);
+
+        //Constructor
+        DiceBag()
+        {
+            rand = new Random(Guid.NewGuid().GetHashCode());
+            log = null;
+        }
+        
+        //Function deffinitions
+        public int Roll(int d)
+        {
+            return rand.Next(1, d);
+        }
+
+        public int Roll(int d, int n)
+        {
+            int total = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                total += rand.Next(1, d);
+            }
+            return total;
+        }
+
+        public int RollMod(int d, int mod)
+        {
+            return rand.Next(1, d) + mod;
+        }
+
+        public int RollMod(int d, int n) 
+        {
+            int total = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                total += rand.Next(1, d);
+            }
+            return total + mod;
+        }
+
+
+
+    }
+}
